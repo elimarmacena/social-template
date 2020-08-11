7d0d3b2c29de090214b5e2602b7c944ebacee212
elimarmacena <elimar.macena@gmail.com>
2020-08-11 00:03:42 -0300

    ADD: user registre and login layout inserted

---
+++ SocialTemplate/app/src/main/java/com/example/socialtemplate/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialtemplate.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.socialtemplate.R;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
