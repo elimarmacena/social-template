ebe207f124aeacf1fc8c954b0781cfa29e06bcae
elimarmacena <elimar.macena@gmail.com>
2020-08-12 11:56:03 -0300

    UPDATE: finished the app navigation

---
+++ SocialTemplate/app/src/main/java/com/example/socialtemplate/activity/PostCreationActivity.java
@@ -0,0 +1,30 @@
+package com.example.socialtemplate.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.socialtemplate.R;
+
+public class PostCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_creation);
+
+        final Context context = this;
+        Button publishButton = findViewById(R.id.publishButton);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- SocialTemplate/app/src/main/java/com/example/socialtemplate/adapter/PostsAdapter.java
+++ SocialTemplate/app/src/main/java/com/example/socialtemplate/adapter/PostsAdapter.java
@@ -1,22 +1,28 @@
 package com.example.socialtemplate.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.socialtemplate.R;
+import com.example.socialtemplate.activity.HomeActivity;
+import com.example.socialtemplate.activity.PostCreationActivity;
 import com.example.socialtemplate.model.ItemView;
 import com.example.socialtemplate.model.PersonPost;
 import com.example.socialtemplate.model.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.File;
 import java.util.List;
@@ -63,13 +69,33 @@ public class PostsAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        BottomNavigationView contexMenu = ((HomeActivity) this.context).findViewById(R.id.bottonMenu);
+        int discoveryPostId = ((HomeActivity) this.context).findViewById(R.id.worldItem).getId();
+        int timeLinePostId = ((HomeActivity) this.context).findViewById(R.id.timelineItem).getId();
+        int selectedItemId = contexMenu.getSelectedItemId();
+        if(selectedItemId == discoveryPostId && (holder.getItemViewType() == 2 || holder.getItemViewType() == 3)){
+            holder.itemView.findViewById(R.id.btnFollow).setBackground(this.context.getDrawable(R.drawable.ic_white_follow));
+        }
+        else if (selectedItemId == timeLinePostId && (holder.getItemViewType() == 2 || holder.getItemViewType() == 3)){
+            holder.itemView.findViewById(R.id.btnFollow).setBackground(this.context.getDrawable(R.drawable.ic_white_unfollow));
+        }
+
+
         // Post creation preview
         switch (holder.getItemViewType()){
             case 1:
                 Bitmap thumbProfile = ((Post)this.homeContent.get(position)).getProfilePhoto();
                 ImageView profilePic = holder.itemView.findViewById(R.id.imageView);
+                final Context localContext = this.context;
                 EditText textInput = holder.itemView.findViewById(R.id.textInputEditText);
-                textInput.setEnabled(false);
+                textInput.setEnabled(true);
+                textInput.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(localContext, PostCreationActivity.class);
+                        localContext.startActivity(intent);
+                    }
+                });
                 profilePic.setImageBitmap(thumbProfile);
                 // Create the intent for the post screen
                 break;
